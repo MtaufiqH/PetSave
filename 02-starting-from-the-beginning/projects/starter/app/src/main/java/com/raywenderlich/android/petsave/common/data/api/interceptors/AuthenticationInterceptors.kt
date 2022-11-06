e3b4d8ebd4f28022ef55af6bdbbd0b0202f87aa7
Muhammad_118359 <adl.m.taufiq@xl.co.id>
2022-11-06 11:29:47 +0800

    Finishing autorization log

--- 02-starting-from-the-beginning/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/api/interceptors/AuthenticationInterceptors.kt
+++ 02-starting-from-the-beginning/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/api/interceptors/AuthenticationInterceptors.kt
@@ -2,9 +2,19 @@ package com.raywenderlich.android.petsave.common.data.api.interceptors
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.raywenderlich.android.petsave.common.data.api.ApiConstants.AUTH_ENDPOINT
+import com.raywenderlich.android.petsave.common.data.api.ApiConstants.KEY
+import com.raywenderlich.android.petsave.common.data.api.ApiConstants.SECRET
 import com.raywenderlich.android.petsave.common.data.api.ApiParameters.AUTH_HEADER
+import com.raywenderlich.android.petsave.common.data.api.ApiParameters.CLIENT_ID
+import com.raywenderlich.android.petsave.common.data.api.ApiParameters.CLIENT_SECRET
+import com.raywenderlich.android.petsave.common.data.api.ApiParameters.GRANT_TYPE_KEY
+import com.raywenderlich.android.petsave.common.data.api.ApiParameters.GRANT_TYPE_VALUE
 import com.raywenderlich.android.petsave.common.data.api.ApiParameters.TOKEN_TYPE
+import com.raywenderlich.android.petsave.common.data.api.model.ApiToken
 import com.raywenderlich.android.petsave.common.data.preferences.PetSavePreferences
+import com.squareup.moshi.Moshi
+import okhttp3.FormBody
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -22,20 +32,82 @@ class AuthenticationInterceptors @Inject constructor(
 
         val interceptedRequest: Request
         if (tokenExpireTime.isAfter(Instant.now())) {
-            // still valid token
+            // still valid token, proceed request
             interceptedRequest = chain.createAuthRequest(currentToken)
         } else {
-            // token expired, request new.
+            // token expired, request new/refresh.
             val tokenRefresh = chain.refreshToken()
+            interceptedRequest = if (tokenRefresh.isSuccessful) {
+                val newToken = mapToken(tokenRefresh)
+                if (newToken.isValid()) {
+                    storeNewToken(newToken)
+                    chain.createAuthRequest(newToken.accessToken.orEmpty())
+                } else {
+                    request
+                }
+            } else {
+                request
+            }
         }
 
+        return chain.proceedDeletingTokenIfUnAuthorized(interceptedRequest)
+
+    }
+
+    private fun storeNewToken(newToken: ApiToken) {
+        preferences.apply {
+            putToken(newToken.accessToken.orEmpty())
+            putTokenType(newToken.tokenType.orEmpty())
+            putTokenExpirationTime(newToken.expiresAt)
+        }
+    }
+
+    private fun mapToken(tokenRefresh: Response): ApiToken {
+        val moshi = Moshi.Builder().build()
+        val tokenAdapter = moshi.adapter<ApiToken>(ApiToken::class.java)
+        val responseBody = tokenRefresh.body
+
+        return tokenAdapter.fromJson(responseBody?.string()) ?: ApiToken.INVALID
+
+    }
+
+
+    companion object {
+        const val UNAUTHORIZED = 401
+    }
+
+    private fun Interceptor.Chain.proceedDeletingTokenIfUnAuthorized(interceptedRequest: Request): Response {
+        val response = proceed(interceptedRequest)
+        if (response.code == UNAUTHORIZED) {
+            preferences.deleteTokenInfo()
+        }
+        return response
+    }
+
+    private fun Interceptor.Chain.refreshToken(): Response {
+        val url = request()
+            .url
+            .newBuilder(AUTH_ENDPOINT)!!
+            .build()
+
+        val body =
+            FormBody.Builder().add(GRANT_TYPE_KEY, GRANT_TYPE_VALUE).add(CLIENT_ID, KEY)
+                .add(CLIENT_SECRET, SECRET).build()
+
+        val tokenRefresh = request()
+            .newBuilder()
+            .post(body)
+            .url(url)
+            .build()
+
+        return proceedDeletingTokenIfUnAuthorized(tokenRefresh)
+    }
+
+    private fun Interceptor.Chain.createAuthRequest(currentToken: String): Request {
+        return request().newBuilder().addHeader(AUTH_HEADER, TOKEN_TYPE + currentToken).build()
     }
 }
 
-private fun Interceptor.Chain.refreshToken(): Response {
 
-}
 
-private fun Interceptor.Chain.createAuthRequest(currentToken: String): Request {
-    return request().newBuilder().addHeader(AUTH_HEADER, TOKEN_TYPE + currentToken).build()
-}
+
